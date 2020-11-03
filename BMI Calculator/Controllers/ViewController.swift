ab46c5d645ac5c3f3b077f2da7dd7d6467b1e9d3
Frederick PORT <developpeur@stud-ios.fr>
2020-11-03 09:16:41 +0100

    connexion des spiders au view contrôler, mise a jour des labels

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var heightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightValueLabel.text = "\(height)m"
+    }
+    
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightValueLabel.text = "\(weight)Kg"
+    }
+    
 
 }
 
